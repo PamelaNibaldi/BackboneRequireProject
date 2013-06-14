7a1c033664a772acb065a95d08978b8401ae7cc9
Pamela Nibaldi <pamela.nibaldi@globant.com>
2013-06-14 12:53:06 -0300

    .

--- js/views/users/edit.js
+++ js/views/users/edit.js
@@ -4,60 +4,52 @@ define([
   'backbone',
   'models/users',
   'collections/users',
-  'text!../../../templates/users/create.html'
-], function($, _, Backbone, User, UsersCollection, usersCreateTemplate){
+  'text!../../../templates/users/edit.html'
+], function($, _, Backbone, User, UsersCollection, usersEditTemplate){
   var messageDisplay = function(msg, classToAdd) {
     var $messageEl = $('.updateMsg');
     $messageEl.html(msg);
     $messageEl.addClass(classToAdd);
     $messageEl.removeClass('notShow');
-    setTimeout(function() {
-        $messageEl.addClass('notShow');
-        $messageEl.removeClass(classToAdd);
+    setTimeout (function() {
+      $messageEl.addClass('notShow');
+      $messageEl.removeClass(classToAdd);
     }, 3000);
   };
-
   var UsersEditView = Backbone.View.extend({
-
     el: $('#content'),
-
-    template: _.template(usersCreateTemplate),
-
+    template: _.template(usersEditTemplate),
     events: {
-      'click .createBtn': 'saveUser'
+      'click .editBtn': 'editUser'
     },
 
-    saveUser: function () {
+    editUser: function () {
+      var id = $('.editBtn').attr('id');
+      var name = $('input[name="firstName"]').val();
+      var lastName = $('input[name="lastName"]').val();
+      var age = $('input[name="age"]').val();
       var user = new User();
-      var $name = $('input[name="firstName"]');
-      var $lastName = $('input[name="lastName"]');
-      var $age = $('input[name="age"]');
-      var localStorage = UsersCollection.localStorage.findAll();
-      var total = localStorage.length;
-      var id = total + 1;
-
       user.on('invalid', function(model, error) {
         var msg = 'There were errors!!!<br>' + error.reduce(function(el, el2) {
           return el + ' <br>' + el2;
         });
         messageDisplay(msg, 'error');
       });
-
-      user.set({id: id , name: $name.val(), lastName: $lastName.val(), age: $age.val()});
-      UsersCollection.add(user);
-      if (user.save()) { //if user was inserted
-        $name.val('');
-        $lastName.val('');
-        $age.val('');
-        messageDisplay('User was successfully added into the database!'); //displays notification message
+      if(user.set({id: id, name: name, lastName: lastName, age: age}, {validate:true})) {
+        localStorage.setItem('users-local-storage-'+id, JSON.stringify(user));
+        messageDisplay('User was successfully edited!'); //displays notification message
+        document.location.href = '';
       }
     },
 
     render: function(id) {
-      compiledTemplate = this.template({});
-      this.$el.html(compiledTemplate);
+      var compiledTemplate;
+      if(id) {
+        var userData = localStorage.getItem('users-local-storage-'+id);
+        compiledTemplate = this.template(JSON.parse(userData));
+        this.$el.html(compiledTemplate);
+      }
     }
-
   });
   return UsersEditView;
 });
