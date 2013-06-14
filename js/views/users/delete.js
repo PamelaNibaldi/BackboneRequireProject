243fa5f7b400d8fdff2c12efd2d477d68488b6df
Dario Cabrera <dario.cabrera@globant.com>
2013-06-14 11:44:28 -0300

    delete.js

--- js/views/users/delete.js
+++ js/views/users/delete.js
@@ -10,7 +10,7 @@ define([
 //  'text!templates/users/delete.html'
 ], function($, _, Backbone, User, UsersCollection, Router, usersDeleteTemplate){
   var UsersDeleteView = Backbone.View.extend({
-    el: $('#content'),
+    el: $('#container'),
     template: _.template(usersDeleteTemplate),
 //    template: _.template( $( '#deleteTemplate').html() ),
     events: {
@@ -44,44 +44,3 @@ define([
   });
   return UsersDeleteView;
 });
-
-
-/*
-define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'models/users',
-  'collections/users',
-  'text!templates/users/delete.html'
-], function($, _, Backbone, User, UsersCollection, usersEditTemplate){
-  var UsersDeleteView = Backbone.View.extend({
-    el: $('#container'),
-//    template: _.template( $( '#deleteTemplate').html() ),
-    template: _.template(usersEditTemplate),
-    events: {
-      'click #editBtn': 'editUser'
-    },
-    deleteUser: function () {
-
-      var id = $('#id').val();
-      window.localStorage.removeItem( "users-local-storage-2" );
-
-    },
-
-    render: function(id) {
-      var compiledTemplate;
-      if(id) {
-        var userData = localStorage.getItem('users-local-storage-'+id);
-        console.log(JSON.parse(userData));
-        var variables = userData;
-        compiledTemplate = this.template(JSON.parse(userData), JSON.parse(userData));
-//        console.log( compiledTemplate );
-//        this.$el.html( this.template( JSON.parse(userData) ) );
-        this.$el.html(compiledTemplate);
-      }
-    }
-  });
-  return UsersDeleteView;
-});
-*/
